0fad9c166a074dea7f8efb2b2462139d427230ac
iphonesector-hash <iphone.sector@gmail.com>
2026-08-21 22:17:07 +0330

    fix(pwa): precache iTunes music provider module

--- sw.js
+++ sw.js
@@ -51,6 +51,7 @@ const PRECACHE_URLS = [
     './src/services/CoupleService.js',
     './src/services/ChatService.js',
     './src/services/MusicService.js',
+    './src/services/ItunesMusicProvider.js',
     './src/services/NotificationService.js',
     './src/services/SoundService.js',
     './src/onboarding/OnboardingFlow.js',
